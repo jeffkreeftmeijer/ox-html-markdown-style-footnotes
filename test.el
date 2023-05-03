08b48c8e007b88c94338b18c41859b878e074161
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-05-03 18:54:22 +0200

    Add footnote-test

---
+++ test.el
@@ -0,0 +1,12 @@
+(require 'ert)
+(load-file "ox-html-markdown-style-footnotes.el")
+
+(ert-deftest footnote-test ()
+  (org-html-markdown-style-footnotes-add)
+  (find-file "test/fixtures/footnote.org")
+  (let ((org-html-stable-ids t))
+    (org-html-export-as-html))
+  (should (string-match-p
+	   "<ol>\n<li class=\"footdef\" id=\"fn.1\"><div class=\"footpara\" role=\"doc-footnote\"><p class=\"footpara\">\nA footnote.\n</p></div><a href=\"#fnr.1\" role=\"doc-backlink\">↩</a></li>\n</ol>"
+	   (with-current-buffer "*Org HTML Export*" (buffer-string))))
+  (org-html-markdown-style-footnotes-add))
