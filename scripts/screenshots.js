05d7538f3e1eebdc2bd49caca7d7d417bd14a38b
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-05-10 11:43:33 +0200

    Add screenshots section

---
+++ scripts/screenshots.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.setViewport({
+    width: 800,
+    height: 200,
+    deviceScaleFactor: 4
+  });
+
+  await page.goto(`file://${__dirname}/../test/fixtures/footnote.html`);
+  await page.waitForSelector('body');
+  body = await page.$('body');
+  await body.screenshot({path: "./before.png"});
+
+  await page.goto(`file://${__dirname}/../test/fixtures/footnote-2.html`);
+  await page.waitForSelector('body');
+  body = await page.$('body');
+  await body.screenshot({path: "./after.png"});
+
+  await page.close();
+  await browser.close();
+})()
